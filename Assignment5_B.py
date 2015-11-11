ff541838c1c622e1745b621278411dbef95fd454
shlshh <0908160@hr.nl>
2015-11-11 01:18:22 +0100

    Added new

---
+++ Assignment5_B.py
@@ -0,0 +1,46 @@
+﻿password = raw_input("What is your Password? ")
+
+if len(password) >12:
+    print 'Password is Strong'
+
+elif len(password) <6:
+    print 'Password is Weak'
+
+else:
+    print 'Password is Medium'
+
+if password.isupper():
+    print "Your Password only contains capital letters"
+
+if password.islower():
+    print "Your Password only contains lower letters"
+
+if password.isdigit():
+    print "Your Password only contains numbers"
+
+if '!' in password:
+    print ""
+
+elif '@' in password:
+    print ""
+    
+elif '#' in password:
+    print ""
+
+elif '$' in password:
+    print ""
+
+elif '%' in password:
+    print ""
+
+elif '^' in password:
+    print ""
+
+elif '&' in password:
+    print ""
+
+elif '*' in password:
+    print ""
+
+else:
+    print "Your Password does not contain a special charactar"
